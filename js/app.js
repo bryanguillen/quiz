bff1dd09cb36454428ba4b27d35f97bf486c967c
Bryan <artificialknowledge217@gmail.com>
2017-03-24 03:03:28 -0400

    finish adding questions

--- js/app.js
+++ js/app.js
@@ -6,8 +6,7 @@ var questionsState = {
 		' mph consistently, how long would it take you to' +
 		' drive just about 60 miles?',
 		answer: '60 minutes',
-		choices: ['45 minutes', '2 hours', '60 minutes', '10 minutes'],
-		rendered: false
+		choices: ['45 minutes', '2 hours', '60 minutes', '10 minutes']
 	},
 	{
 		question: 'You are in an elevator with 3 other people.' + 
@@ -16,10 +15,62 @@ var questionsState = {
 		'weigh 100 pounds. The elevator\'s max capacity is 650 lbs.' + 
 		' What weight should the person be, if you want to get stuck in the elevator as an excuse to miss work?',
 		answer: '150 lbs',
-		choices: ['145 lbs', '150 lbs', '170 lbs', '200 lbs'],
-		rendered: false
+		choices: ['145 lbs', '150 lbs', '170 lbs', '200 lbs']
 	},
-	
+	{
+		question: 'You walk west 3 miles and then south 4 miles. ' +
+		'Instead of taking that long way, you realize that the quickest way from point ' + 
+   		'A to point B is a straight line. If you take quickest way, how many miles would you then walk?',
+   		answer: '5 miles',
+   		choices: ['10 miles', '4 miles', '3 miles', '5 miles']
+	},
+	{
+		question: 'You have 3 siblings. One is 5 foot 8 inches tall, ' +
+		'another 6 foot tall, another 5 foot 5 tall, and you 6 foot tall. Your parents ' +
+   		'are having another baby after 20 years. About how tall should you expect this new baby to be when its your age?',
+   		answer: '5 foot 9 inches',
+   		choices: ['5 foot 9 inches', '6 foot 2 inches', '5 foot 6 inches', '5 foot 7 inches']
+	},
+	{
+		question: 'You weigh 180 lbs, and you need to be under 167 lbs, ' +
+		'but over 160 lbs, to make weight for your big wrestling match. How many ' +
+   		'pounds should you at least lose?',
+   		answer: '14 lbs',
+   		choices: ['14 lbs', '20 lbs', '17 lbs', '200 lbs']
+	},
+	{
+		question: 'You have 10,000 dollars in the bank on the first day of the month. ' +
+		'And now, you need to pay rent and other expenses. ' + 
+   		'Your rent is 1500 dollars a month, you eat 200 $ worth of groceries a month, car insurance is 100 $ a month, car ' +
+  		'payment 400$ a month, and at most 200$ for going out and having fun. How much should you budget for the month? ',
+  		answer: '2400 dollars',
+  		choices: ['1700 dollars', '2400 dollars', '2100 dollars', '2200 dollars']
+	},
+	{
+		question: 'Your car can take 10 gallons of gas, in order to fill up. ' +
+		'If your car is on E (empty) and gas is 2.50$, how much will it cost ' +
+   		'to fill up your car?',
+   		answer: '25.00 $',
+   		choices: ['20.00 $', '15.00 $', '25.00 $', '30.00 $']
+	},
+	{
+		question: 'At the grocery store you see fruits and vegetables on special. ' +
+		'You zoom in on lemons because you need them. Good news, the lemons ' +
+   		'are on special, 10 lemons for 3$. Is this really a special. How much is each lemon going to cost you?',
+   		answer: '0.30 $',
+   		choices: ['I HAVE NO CLUE', '0.03 $', '0.30 $', 'NONE OF THE ABOVE']
+	},
+	{
+		question: 'How many degrees does a car tire roatate while driving?',
+		answer: '360',
+		choices: ['WHAT? Obviously None Of These', '360', '180', '0']
+	},
+	{
+		question: 'If you get two 90s and an 80 on tests, what must you get in order on the last test ' +
+   		'in order to get a 90 average for the class.',
+   		answer: '100',
+   		choices: ['100', '95', '80', 'None Of The Above']
+	}
 	],
 	//default value of the question number
 }
@@ -125,7 +176,7 @@ function renderQuestion(element, question) {
 function renderInstructions(currentTarget) {
 	currentTarget.toggleClass('js-start-button').addClass('js-got-it');
 	currentTarget.text('Got It!');
-	currentTarget.closest('main').find('span').text('Just Simply Click Answer And Submit!');
+	currentTarget.closest('main').find('span').text('Just Simply Click Answer And Scroll Down & Submit!');
 	currentTarget.closest('main').find('h1').text('Quiz Rules');
 }
 
